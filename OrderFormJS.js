6a143a6daadc07e1474c9f9d4904b230ff231736
penguinmaan <penguinmaan@gmail.com>
2017-08-27 04:05:33 -0700

    Minor fix to fade-in
    Makes sure it does a check when page loads (in case user refreshes in
    the middle)

--- OrderFormJS.js
+++ OrderFormJS.js
@@ -1,11 +1,21 @@
 $(document).ready(function() {
-	$(".gallery").on('click', function() {
-		$(this).parent().children("input:first").val(function(i, oldval) {
-    		return parseInt( oldval, 10) + 1;
-			});
-	});
 
-	    $(window).scroll( function(){
+	$('.introText').each( function(i){
+            
+        var bottom_of_object = $(this).offset().top + 200;
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+        /* If the object is completely visible in the window, fade it it */
+        if( bottom_of_window > bottom_of_object ){
+                
+            $(this).addClass('fadein');
+                    
+        }
+            
+    }); 
+
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
     
         /* Check the location of each desired element */
         $('.introText').each( function(i){
@@ -23,4 +33,11 @@ $(document).ready(function() {
         }); 
     
     });
+
+    $(".gallery").on('click', function() {
+		$(this).parent().children("input:first").val(function(i, oldval) {
+    		return parseInt( oldval, 10) + 1;
+			});
+	});
+    
 });
--- indexJS.js
+++ indexJS.js
@@ -1,5 +1,19 @@
 $(document).ready(function() {
     
+    $('.introText').each( function(i){
+            
+        var bottom_of_object = $(this).offset().top + 200;
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+        /* If the object is completely visible in the window, fade it it */
+        if( bottom_of_window > bottom_of_object ){
+                
+            $(this).addClass('fadein');
+                    
+        }
+            
+    }); 
+    
     /* Every time the window is scrolled ... */
     $(window).scroll( function(){
     
